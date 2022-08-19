701f2496afab4b602a59e0f15c8afb7618e26b0e
sidsitaula <siddharthasitaula02@gmail.com>
2022-08-19 23:14:21 +0545

    Added file 0

--- ccgg.py
+++ ccgg.py
@@ -1,4 +1,18 @@
 import os
 import time
+i=0
+
+#checking out to main branch
+os.system("git checkout main")
 
 #write main code here 
+while(i<=10):
+    
+
+    os.system(f"touch pfile_{i}.py")
+    os.system(f"git add .")
+    os.system(f'git commit -m "Added file {i}"')
+    os.system(f"git push origin main")
+    
+    #halka rest
+    time.sleep(2)
